f1f4f464af85266f22a9b6e4ea8194a0a5542f9d
davidojo1144 <davco1144@gmail.com>
2025-04-24 14:12:46 +0100

    Talent Showcase App using typescript, tailwind css, framer motion, toastify

---
+++ Talentapp/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1rem",
+          lg: "1rem",
+          xl: "4rem",
+          "2xl": "5rem",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+
+
+
+
