d2bd9d0980a1f69acb3855ca8a9ba753441aaf84
davidojo1144 <davco1144@gmail.com>
2025-04-24 16:53:09 +0100

    Talent Showcase App using typescript, tailwind css, framer motion, toastify

--- Talentapp/src/components/Footer.tsx
+++ Talentapp/src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <p  className='text-sm font-light'>Learning is the key to growth—it <br/>empowers you to adapt, overcome challenges, <br/>and unlock new opportunities. Every skill you <br/>gain and every idea you explore strengthens <br/>your future. Knowledge builds confidence, fuels <br/>progress, and keeps your mind sharp. <br/>No matter where life takes you, learning <br/>ensures you’re always moving forward.</p>
         </div>
         <div>
-          <p className='md:text-xl text-lg '>Products</p>
+          <p className='md:text-xl text-lg md:pb-5 pb-2'>Products</p>
           <div className='text-sm font-light'>
             <p>Writing & Note-Taking</p>
             <p>Reading & Reference</p>
@@ -21,7 +21,7 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <p className='md:text-xl text-lg '>Useful Links</p>
+          <p className='md:text-xl text-lg md:pb-5 pb-2'>Useful Links</p>
           <div className='text-sm font-light'>
             <p className='cursor-pointer' href="#">Blogs</p>
             <p className='cursor-pointer' href="#">Pricing</p>
@@ -31,11 +31,11 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <p className='md:text-xl text-lg '>Address</p>
+          <p className='md:text-xl text-lg md:pb-5 pb-2'>Address</p>
           <p className='text-sm font-light'>145, Mark way <br/>Sabo Yaba <br/> Lagos State, Nigeria.</p>
         </div>
       </div>
-      <p className='text-center text-secondary pt-10'>All Rights Reserved by © Learn Now 2025</p>
+      <p className='text-center text-secondary pt-10'>All Rights Reserved by © Skill Link 2025</p>
     </footer>
   )
 }
--- Talentapp/src/components/Hero.tsx
+++ Talentapp/src/components/Hero.tsx
@@ -9,8 +9,8 @@ const Hero = () => {
         <div className="space-y-3">
             <h2 className="text-center prata-regular md:text-3xl text-xl font-medium">Skill Link</h2>
             <div className="space-y-2">
-              <p className="md:text-lg text-sm md:font-light">Talent Showcase App – Where talent shines, connections thrive, and opportunities come to life! ✨ Showcase your skills, inspire the world, and build your future—one brilliant ability at a time.</p>
-              <p className="md:text-lg text-sm md:font-light">Where talent meets opportunity. Showcase your skills, connect with the right audience, and turn your abilities into success—effortlessly."</p>
+              <p className="text-lg md:font-light">Talent Showcase App – Where talent shines, connections thrive, and opportunities come to life! ✨ Showcase your skills, inspire the world, and build your future—one brilliant ability at a time.</p>
+              <p className="text-lg md:font-light">Where talent meets opportunity. Showcase your skills, connect with the right audience, and turn your abilities into success—effortlessly."</p>
             </div>
         </div>
         <AuthForm/>
