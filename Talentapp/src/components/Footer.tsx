f06b4fd5ea86fc44d49277ce9c7f668dc0b2bf87
davidojo1144 <davco1144@gmail.com>
2025-04-24 16:45:46 +0100

    Talent Showcase App using typescript, tailwind css, framer motion, toastify

---
+++ Talentapp/src/components/Footer.tsx
@@ -0,0 +1,43 @@
+
+
+
+const Footer = () => {
+  return (
+    <footer className='mt-44 pt-10  pb-10'>
+      <div className='flex md:flex-row flex-col md:items-center md:space-y-0 space-y-16 justify-between container text-white'>
+        <div>
+          <p  className='text-sm font-light'>Learning is the key to growth—it <br/>empowers you to adapt, overcome challenges, <br/>and unlock new opportunities. Every skill you <br/>gain and every idea you explore strengthens <br/>your future. Knowledge builds confidence, fuels <br/>progress, and keeps your mind sharp. <br/>No matter where life takes you, learning <br/>ensures you’re always moving forward.</p>
+        </div>
+        <div>
+          <p className='md:text-xl text-lg '>Products</p>
+          <div className='text-sm font-light'>
+            <p>Writing & Note-Taking</p>
+            <p>Reading & Reference</p>
+            <p>Digital Learning Tools</p>
+            <p>Math & Science Supplies</p>
+            <p>Creativity & Projects</p>
+            <p>Organization & Study Aids</p>
+            <p>Comfort & Focus</p>
+          </div>
+        </div>
+        <div>
+          <p className='md:text-xl text-lg '>Useful Links</p>
+          <div className='text-sm font-light'>
+            <p className='cursor-pointer' href="#">Blogs</p>
+            <p className='cursor-pointer' href="#">Pricing</p>
+            <p className='cursor-pointer' href="#">Certifications</p>
+            <p className='cursor-pointer' href="#">Reviews</p>
+            <p className='cursor-pointer' href="#">Success Stories</p>
+          </div>
+        </div>
+        <div>
+          <p className='md:text-xl text-lg '>Address</p>
+          <p className='text-sm font-light'>145, Mark way <br/>Sabo Yaba <br/> Lagos State, Nigeria.</p>
+        </div>
+      </div>
+      <p className='text-center text-secondary pt-10'>All Rights Reserved by © Learn Now 2025</p>
+    </footer>
+  )
+}
+
+export default Footer
--- Talentapp/src/components/Hero.tsx
+++ Talentapp/src/components/Hero.tsx
@@ -5,12 +5,12 @@ const Hero = () => {
   return (
     <div className="container">
       <div className="flex md:flex-row flex-col pt-20 items-center justify-center gap-10">
-        <img className="md:w-56 w-full md:h-80 rounded-xl" src={assets} alt="" />
-        <div className="space-y-5">
+        <img className="md:w-56 w-full md:h-80 h-[60vh] rounded-xl" src={assets} alt="" />
+        <div className="space-y-3">
             <h2 className="text-center prata-regular md:text-3xl text-xl font-medium">Skill Link</h2>
-            <div className="space-y-3">
-              <p>Talent Showcase App – Where talent shines, connections thrive, and opportunities come to life! ✨ Showcase your skills, inspire the world, and build your future—one brilliant ability at a time.</p>
-              <p>Where talent meets opportunity. Showcase your skills, connect with the right audience, and turn your abilities into success—effortlessly."</p>
+            <div className="space-y-2">
+              <p className="md:text-lg text-sm md:font-light">Talent Showcase App – Where talent shines, connections thrive, and opportunities come to life! ✨ Showcase your skills, inspire the world, and build your future—one brilliant ability at a time.</p>
+              <p className="md:text-lg text-sm md:font-light">Where talent meets opportunity. Showcase your skills, connect with the right audience, and turn your abilities into success—effortlessly."</p>
             </div>
         </div>
         <AuthForm/>
