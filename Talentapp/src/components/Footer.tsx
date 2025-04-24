160a7735cd3717282db25efcb2784f37d545bb5b
davidojo1144 <davco1144@gmail.com>
2025-04-24 17:00:51 +0100

    Talent Showcase App using typescript, tailwind css, framer motion, toastify

--- Talentapp/src/components/Footer.tsx
+++ Talentapp/src/components/Footer.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
     <footer className='mt-44 pt-10  pb-10'>
       <div className='flex md:flex-row flex-col md:items-center md:space-y-0 space-y-16 justify-between container text-white'>
         <div>
-          <p  className='text-sm font-light'>Learning is the key to growth—it <br/>empowers you to adapt, overcome challenges, <br/>and unlock new opportunities. Every skill you <br/>gain and every idea you explore strengthens <br/>your future. Knowledge builds confidence, fuels <br/>progress, and keeps your mind sharp. <br/>No matter where life takes you, learning <br/>ensures you’re always moving forward.</p>
+          <p  className='text-sm text-center md:text-start font-light'>"Skills are your superpower—they help you adapt, <br/>stand out, and seize new opportunities. Every talent<br/> you master and every ability you showcase on SkillLink<br/> opens doors. Confidence grows with each skill shared, <br/>progress accelerates with every connection made, and <br/>your potential becomes limitless. No matter your goals, <br/>SkillLink ensures your abilities are seen, valued, and rewarded. 🚀"</p>
         </div>
         <div>
-          <p className='md:text-xl text-lg md:pb-5 pb-2'>Products</p>
-          <div className='text-sm font-light'>
+          <p className='md:text-xl text-center md:text-start  text-lg md:pb-5 pb-2'>Products</p>
+          <div className='text-sm text-center md:text-start  font-light'>
             <p>Writing & Note-Taking</p>
             <p>Reading & Reference</p>
             <p>Digital Learning Tools</p>
@@ -21,8 +21,8 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <p className='md:text-xl text-lg md:pb-5 pb-2'>Useful Links</p>
-          <div className='text-sm font-light'>
+          <p className='md:text-xl text-center md:text-start  text-lg md:pb-5 pb-2'>Useful Links</p>
+          <div className='text-sm text-center md:text-start  font-light'>
             <p className='cursor-pointer' href="#">Blogs</p>
             <p className='cursor-pointer' href="#">Pricing</p>
             <p className='cursor-pointer' href="#">Certifications</p>
@@ -31,8 +31,8 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <p className='md:text-xl text-lg md:pb-5 pb-2'>Address</p>
-          <p className='text-sm font-light'>145, Mark way <br/>Sabo Yaba <br/> Lagos State, Nigeria.</p>
+          <p className='md:text-xl text-center md:text-start  text-lg md:pb-5 pb-2'>Address</p>
+          <p className='text-sm text-center md:text-start  font-light'>145, Mark way <br/>Sabo Yaba <br/> Lagos State, Nigeria.</p>
         </div>
       </div>
       <p className='text-center text-secondary pt-10'>All Rights Reserved by © Skill Link 2025</p>
