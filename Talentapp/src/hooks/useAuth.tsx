b461d7dc9418518397588151cfc953071d367ae7
davidojo1144 <davco1144@gmail.com>
2025-04-25 12:26:03 +0100

    Talent Showcase App using typescript, tailwind css, framer motion, toastify, Supabase

--- Talentapp/src/hooks/useAuth.tsx
+++ Talentapp/src/hooks/useAuth.tsx
@@ -12,12 +12,6 @@ export const useAuth = () => {
   useEffect(() => {
     const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
       setUser(session?.user ?? null);
-      // if (event === 'SIGNED_IN') {
-      //   toast.success(`Welcome back, ${session?.user.email}!`);
-      // }
-      // if (event === 'SIGNED_OUT') {
-      //   toast.info('You have been logged out');
-      // }
     });
 
     return () => subscription.unsubscribe();
@@ -35,21 +29,23 @@ export const useAuth = () => {
 
   const signIn = async (email: string, password: string): Promise<AuthResponse> => {
     const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-    if (!error) {
-      toast.success(`Welcome back, ${data.user?.email}!`);
-    } else {
-      toast.error(`Login failed: ${error.message}`);
-    }
+    setTimeout(()=> {
+      if (!error) {
+        toast.success(`Welcome back, ${data.user?.email}!`);
+      } else {
+        toast.error(`Login failed: ${error.message}`);
+      }
+    }, 50)
     return { user: data.user, error };
   };
 
   const signOut = async (): Promise<{ error: Error | null }> => {
     const { error } = await supabase.auth.signOut();
-    // if (!error) {
-    //   toast.info('You have been logged out');
-    // } else {
-    //   toast.error(`Logout failed: ${error.message}`);
-    // }
+    if (!error) {
+      toast.info('You have been logged out');
+    } else {
+      toast.error(`Logout failed: ${error.message}`);
+    }
     return { error };
   };
 
